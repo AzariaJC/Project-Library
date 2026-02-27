7d338b2c2eb1e143ec37299d45dddcfb100d42f7
AJ <45110670+AzariaJC@users.noreply.github.com>
2026-02-27 16:27:41 +0000

    added delete button inside loopThroughLibrary

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const closeBtn = document.querySelector("#close-btn");
 
+
+
 function Book(title, author) {
     this.title = title;
     this.author = author;
@@ -22,9 +24,16 @@ function loopThroughLibrary() {
         const book = myLibrary[i];
         if (!document.getElementById(book.id)) {
             const listItem = document.createElement('li');
+            const deleteButton = document.createElement("button");
             listItem.setAttribute('id', book.id);
             listItem.textContent = `${book.title} by ${book.author}`;
+            deleteButton.textContent = "Delete";
+            deleteButton.addEventListener("click", () => {
+                myLibrary.splice(i, 1);
+                listItem.remove();
+            })
             listContainer.appendChild(listItem);
+            listItem.appendChild(deleteButton);
         }
     }
 }
@@ -36,10 +45,10 @@ bookForm.addEventListener("submit", (e) => {
 
     // 2. Create the new book object (assuming you have a Book constructor)
     const newBook = new Book(titleInput.value, authorInput.value);
-    
+
     // 3. Add it to your array and update the UI
     myLibrary.push(newBook);
-    loopThroughLibrary(); 
+    loopThroughLibrary();
 
     // 4. Reset the form and close the dialog
     bookForm.reset();
@@ -50,3 +59,7 @@ closeBtn.addEventListener("click", (e) => {
     bookForm.reset();
     myDialog.close();
 });
+
+deleteButton.addEventListener("click", (e) => {
+    myLibrary[i]
+})
